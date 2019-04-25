7cc2e75cd52373bb4fa84683b86c5ccdfb540093
Brian Pollack <brianleepollack@gmail.com>
2019-04-25 19:07:20 -0400

    need to flesh out fix for weird z-labels

--- data_process.py
+++ data_process.py
@@ -13,6 +13,12 @@ class DicomToXArray:
         self.metadata_list = []
         self.x_shape = None
         self.y_shape = None
+        self.x_spacing = None
+        self.y_spacing = None
+        self.x_origin = None
+        self.y_origin = None
+        self.zs_type1 = []
+        self.zs_type2 = []
 
         print(self.SAs)
         self.get_images_and_metadata()
@@ -21,14 +27,25 @@ class DicomToXArray:
     def get_images_and_metadata(self):
         for i, SA in enumerate(self.SAs):
             reader, image = self.get_reader_and_image(SA)
+            # if reader.GetMetaData(0, '0018|1312').strip() == 'ROW':
+            #     print('row')
+            #     continue
             if not self.x_shape:
+                # Getting shape, spacing, and origin for X-Y
+                # Using first temporal slice grabbed, ignoring conflicting
+                # info from all other temporal slices
                 self.x_shape = image.GetSize()[0]
                 self.y_shape = image.GetSize()[1]
+                self.x_spacing = image.GetSpacing()[0]
+                self.y_spacing = image.GetSpacing()[1]
+                self.x_origin = image.GetOrigin()[0]
+                self.y_origin = image.GetOrigin()[1]
+                self.direction = image.GetDirection()
             metadata = self.get_metadata(reader, image)
             mask = self.get_mask(SA, image)
 
-            print(i, image.GetSize())
-            image, mask, metadata = self.check_axis(image, mask, metadata)
+            # image, mask, metadata = self.check_axis(image, mask, metadata)
+            image, mask, metadata = self.resample_images(image, mask, metadata)
 
             self.image_list.append(sitk.GetArrayFromImage(image))
             self.mask_list.append(sitk.GetArrayFromImage(mask))
@@ -45,22 +62,27 @@ class DicomToXArray:
         return reader, image
 
     def get_metadata(self, reader, image):
+        # Assume metadata for first temporal slice works for everything
+        # (except temporal slice position)
 
-        xyz = reader.GetMetaData(0, '0020|0032')
+        xyz = reader.GetMetaData(5, '0020|0032')
         xyz = [float(a) for a in xyz.split('\\')]
-        other_z = float(reader.GetMetaData(0, '0020|1041'))
+        other_z = float(reader.GetMetaData(5, '0020|1041'))
         x = xyz[0]
         y = xyz[1]
         # z = other_z
         z = xyz[2]
+        self.zs_type1.append(z)
+        self.zs_type2.append(other_z)
 
         spacing = reader.GetMetaData(0, '0028|0030')
         spacing = [float(a) for a in spacing.split('\\')]
         x_spacing = spacing[0]
         y_spacing = spacing[1]
 
-        t = int(reader.GetMetaData(0, '0020|0013'))
-        print(xyz[0:2], other_z)
+        t = []
+        for i in range(image.GetSize()[-1]):
+            t.append(int(reader.GetMetaData(i, '0020|0013')))
 
         return [x, y, z, t, x_spacing, y_spacing]
 
@@ -79,7 +101,7 @@ class DicomToXArray:
             ref_image = sitk.Cast(ref_image, image.GetPixelIDValue())
             center = sitk.CenteredTransformInitializer(
                 ref_image, image, sitk.Euler3DTransform(),
-                sitk.CenteredTransformInitializerFilter.GEOMETRY
+                sitk.CenteredTransformInitializerFilter.MOMENTS
             )
             new_image = sitk.Resample(image, ref_image, center,
                                       sitk.sitkNearestNeighbor)
@@ -89,6 +111,22 @@ class DicomToXArray:
         else:
             raise RuntimeError(f'Check Axis Failure: true shape:{true_shape},'
                                f' img shape:{im_shape}')
+
+    def resample_images(self, image, mask, metadata):
+        ref_image = sitk.Image(self.x_shape, self.y_shape, image.GetSize()[2], 2)
+        ref_image.SetSpacing((self.x_spacing, self.y_spacing, 1))
+        ref_image.SetOrigin((self.x_origin, self.y_origin, image.GetOrigin()[-1]))
+        ref_image.SetDirection(image.GetDirection())
+        ref_image = sitk.Cast(ref_image, image.GetPixelIDValue())
+        center = sitk.CenteredTransformInitializer(
+            ref_image, image, sitk.AffineTransform(3),
+            sitk.CenteredTransformInitializerFilter.GEOMETRY
+        )
+        new_image = sitk.Resample(image, ref_image, center,
+                                  sitk.sitkNearestNeighbor)
+        new_mask = sitk.Resample(mask, ref_image, center,
+                                 sitk.sitkNearestNeighbor)
+        return new_image, new_mask, metadata
 
     def get_mask(self, SA, image):
         png_names = SA + '/*.png'
@@ -112,10 +150,27 @@ class DicomToXArray:
         print(np.asarray(self.image_list).shape)
         for a in self.metadata_list:
             print(a)
+
+        zs = self.determine_best_zs()
         self.xrds = xr.Dataset({'image': (['z', 't', 'y', 'x'], self.image_list),
                                 'mask': (['z', 't', 'y', 'x'], self.mask_list)},
-                               coords={'t': range(self.image_list[0].shape[0]),
+                               coords={'t': self.metadata_list[0][3],
                                        'x': range(self.x_shape),
                                        'y': range(self.y_shape),
-                                       'z': [a[2] for a in self.metadata_list]})
+                                       # 'z': [a[2] for a in self.metadata_list]})
+                                       'z': zs})
         self.xrds = self.xrds.sortby(['t', 'z'])
+
+    def determine_best_zs(self):
+        spacing_type1 = np.diff(np.sort(self.zs_type1))
+        spacing_type2 = np.diff(np.sort(self.zs_type2))
+        print(np.sort(self.zs_type1))
+        print(np.sort(self.zs_type2))
+
+        print(spacing_type1)
+        print(spacing_type2)
+
+        if np.std(spacing_type1) <= np.std(spacing_type2):
+            return self.zs_type1
+        else:
+            return self.zs_type2
