95229df365182010ef8d5cc9327ed0a02f3e08c7
Brian Pollack <brianleepollack@gmail.com>
2019-04-26 18:56:36 -0400

    updated to be stronger, smarter, and take longer

--- data_process.py
+++ data_process.py
@@ -1,3 +1,4 @@
+import pandas as pd
 import xarray as xr
 import SimpleITK as sitk
 import numpy as np
@@ -8,115 +9,131 @@ class DicomToXArray:
     def __init__(self, patient_dir):
         self.dir = patient_dir
         self.SAs = glob.glob(patient_dir+'/SA*')
+        self.raw_image_dict = {}
         self.image_list = []
         self.mask_list = []
-        self.metadata_list = []
-        self.x_shape = None
-        self.y_shape = None
-        self.x_spacing = None
-        self.y_spacing = None
-        self.x_origin = None
-        self.y_origin = None
-        self.zs_type1 = []
-        self.zs_type2 = []
+        self.metadata_dict = {}
+        self.timestamp_dict = {}
+        self.weirdness_dict = {}
+        self.loc_dict = {}
+        self.slice_dict = {}
+        self.shape_dict = {}
+        self.spacing_dict = {}
+        self.direction_dict = {}
+        self.bad_SAs = set()
+        self.good_SAs = set()
 
         print(self.SAs)
         self.get_images_and_metadata()
         self.make_xarray()
 
-    def get_images_and_metadata(self):
-        for i, SA in enumerate(self.SAs):
-            reader, image = self.get_reader_and_image(SA)
-            # if reader.GetMetaData(0, '0018|1312').strip() == 'ROW':
-            #     print('row')
-            #     continue
-            if not self.x_shape:
-                # Getting shape, spacing, and origin for X-Y
-                # Using first temporal slice grabbed, ignoring conflicting
-                # info from all other temporal slices
-                self.x_shape = image.GetSize()[0]
-                self.y_shape = image.GetSize()[1]
-                self.x_spacing = image.GetSpacing()[0]
-                self.y_spacing = image.GetSpacing()[1]
-                self.x_origin = image.GetOrigin()[0]
-                self.y_origin = image.GetOrigin()[1]
-                self.direction = image.GetDirection()
-            metadata = self.get_metadata(reader, image)
-            mask = self.get_mask(SA, image)
-
-            # image, mask, metadata = self.check_axis(image, mask, metadata)
-            image, mask, metadata = self.resample_images(image, mask, metadata)
-
-            self.image_list.append(sitk.GetArrayFromImage(image))
-            self.mask_list.append(sitk.GetArrayFromImage(mask))
-            self.metadata_list.append(metadata)
-
-    def get_reader_and_image(self, SA):
+    def get_reader(self, SA):
         reader = sitk.ImageSeriesReader()
-        # dicom_names = reader.GetGDCMSeriesFileNames(self.dir, sid)
         dicom_names = reader.GetGDCMSeriesFileNames(SA)
         reader.SetFileNames(dicom_names)
         reader.MetaDataDictionaryArrayUpdateOn()  # Get DICOM Info
         reader.LoadPrivateTagsOn()  # Get DICOM Info
+        return reader
+
+    def get_reader_and_image(self, SA):
+        reader  = self.get_reader(SA)
         image = reader.Execute()
         return reader, image
 
-    def get_metadata(self, reader, image):
-        # Assume metadata for first temporal slice works for everything
-        # (except temporal slice position)
+    def load_metadata(self, SA, reader, slices):
+        # load metadata for first timestamp
+        # (except the timestamp value, need to get that for all)
 
-        xyz = reader.GetMetaData(5, '0020|0032')
-        xyz = [float(a) for a in xyz.split('\\')]
-        other_z = float(reader.GetMetaData(5, '0020|1041'))
-        x = xyz[0]
-        y = xyz[1]
-        # z = other_z
-        z = xyz[2]
-        self.zs_type1.append(z)
-        self.zs_type2.append(other_z)
+        vals = []
+        for k in reader.GetMetaDataKeys(0):
+            vals.append(reader.GetMetaData(0, k))
+        self.metadata_dict[SA] = pd.Series(vals, reader.GetMetaDataKeys(0))
 
-        spacing = reader.GetMetaData(0, '0028|0030')
-        spacing = [float(a) for a in spacing.split('\\')]
-        x_spacing = spacing[0]
-        y_spacing = spacing[1]
+        locs = [float(a) for a in self.metadata_dict[SA]['0020|0032'].split('\\')]
+        self.loc_dict[SA] = locs
 
-        t = []
-        for i in range(image.GetSize()[-1]):
-            t.append(int(reader.GetMetaData(i, '0020|0013')))
+        self.slice_dict[SA] = float(self.metadata_dict[SA]['0020|1041'])
 
-        return [x, y, z, t, x_spacing, y_spacing]
+        spacing = [float(a) for a in self.metadata_dict[SA]['0028|0030'].split('\\')]
+        self.spacing_dict[SA] = spacing
 
-    def check_axis(self, image, mask, metadata):
-        im_shape = list(image.GetSize()[0:2])
-        true_shape = [self.x_shape, self.y_shape]
-        if im_shape == true_shape:
-            return image, mask, metadata
-        # elif im_shape == true_shape[::-1]:
-        elif im_shape != true_shape:
-            ref_image = sitk.Image(self.x_shape, self.y_shape, image.GetSize()[2], 2)
-            print((self.metadata_list[0][4], self.metadata_list[0][5], 1))
-            ref_image.SetSpacing((self.metadata_list[0][4], self.metadata_list[0][5], 1))
-            ref_image.SetOrigin(image.GetOrigin())
-            ref_image.SetDirection(image.GetDirection())
-            ref_image = sitk.Cast(ref_image, image.GetPixelIDValue())
-            center = sitk.CenteredTransformInitializer(
-                ref_image, image, sitk.Euler3DTransform(),
-                sitk.CenteredTransformInitializerFilter.MOMENTS
-            )
-            new_image = sitk.Resample(image, ref_image, center,
-                                      sitk.sitkNearestNeighbor)
-            new_mask = sitk.Resample(mask, ref_image, center,
-                                     sitk.sitkNearestNeighbor)
-            return new_image, new_mask, metadata
-        else:
-            raise RuntimeError(f'Check Axis Failure: true shape:{true_shape},'
-                               f' img shape:{im_shape}')
+        self.timestamp_dict[SA] = []
+        for i in range(slices):
+            self.timestamp_dict[SA].append(int(reader.GetMetaData(i, '0020|0013')))
 
-    def resample_images(self, image, mask, metadata):
-        ref_image = sitk.Image(self.x_shape, self.y_shape, image.GetSize()[2], 2)
-        ref_image.SetSpacing((self.x_spacing, self.y_spacing, 1))
-        ref_image.SetOrigin((self.x_origin, self.y_origin, image.GetOrigin()[-1]))
-        ref_image.SetDirection(image.GetDirection())
+    def calc_weirdness(self):
+        for SA in self.SAs:
+            weirdness = 0
+            for SA_test in self.SAs:
+                if SA != SA_test:
+                    weirdness += np.sum(~self.metadata_dict[SA].eq(self.metadata_dict[SA_test]))
+            self.weirdness_dict[SA] = weirdness
+
+    def mark_bads(self):
+        # mark slices as bad if any exist
+        for SA in self.SAs:
+            for SA_test in self.SAs:
+                if SA_test == SA or (SA_test in self.bad_SAs):
+                    continue
+                too_close = np.isclose(self.loc_dict[SA][-1], self.loc_dict[SA_test][-1], atol=0.5)
+                too_close_slice = np.isclose(self.slice_dict[SA],
+                                             self.slice_dict[SA_test], atol=0.5)
+                if too_close or too_close_slice:
+                    print(SA, SA_test)
+                    print(self.loc_dict[SA], self.loc_dict[SA_test])
+                    if self.weirdness_dict[SA] > self.weirdness_dict[SA_test]:
+                        self.bad_SAs.add(SA)
+                    else:
+                        self.bad_SAs.add(SA_test)
+        print(self.bad_SAs)
+        self.good_SAs = self.bad_SAs.symmetric_difference(self.SAs)
+
+    def get_ideal_params(self):
+        x_shapes = [self.shape_dict[SA][0] for SA in self.good_SAs]
+        y_shapes = [self.shape_dict[SA][1] for SA in self.good_SAs]
+        x_spacings = [self.spacing_dict[SA][0] for SA in self.good_SAs]
+        y_spacings = [self.spacing_dict[SA][1] for SA in self.good_SAs]
+        x_origins = [self.loc_dict[SA][0] for SA in self.good_SAs]
+        y_origins = [self.loc_dict[SA][1] for SA in self.good_SAs]
+        directions = [self.direction_dict[SA] for SA in self.good_SAs]
+
+        self.ideal_x_shape = int(np.mean(x_shapes))
+        self.ideal_y_shape = int(np.mean(y_shapes))
+        self.ideal_x_spacing = np.mean(x_spacings)
+        self.ideal_y_spacing = np.mean(y_spacings)
+        self.ideal_x_origin = np.mean(x_origins)
+        self.ideal_y_origin = np.mean(y_origins)
+        directions = np.asarray(directions)
+        self.ideal_directions = directions.mean(axis=0)
+
+    def get_images_and_metadata(self):
+        for i, SA in enumerate(self.SAs):
+            reader, image = self.get_reader_and_image(SA)
+            self.shape_dict[SA] = image.GetSize()
+            self.direction_dict[SA] = image.GetDirection()
+            self.load_metadata(SA, reader, image.GetSize()[-1])
+            self.raw_image_dict[SA] = image
+
+        self.calc_weirdness()
+        self.mark_bads()
+        self.get_ideal_params()
+
+        print(self.good_SAs)
+        for SA in self.good_SAs:
+            mask = self.get_mask(SA)
+            image, mask = self.resample_images(SA, mask)
+            self.image_list.append(sitk.GetArrayFromImage(image))
+            self.mask_list.append(sitk.GetArrayFromImage(mask))
+
+    def resample_images(self, SA, mask):
+        image = self.raw_image_dict[SA]
+        ref_image = sitk.Image((int(self.ideal_x_shape), int(self.ideal_y_shape),
+                                int(image.GetSize()[-1])), 2)
+        ref_image.SetSpacing((self.ideal_x_spacing, self.ideal_y_spacing, 1))
+        ref_image.SetOrigin((self.ideal_x_origin, self.ideal_y_origin, image.GetOrigin()[-1]))
+        # ref_image.SetOrigin(image.GetOrigin())
+        # ref_image.SetDirection(image.GetDirection())
+        ref_image.SetDirection(self.ideal_directions)
         ref_image = sitk.Cast(ref_image, image.GetPixelIDValue())
         center = sitk.CenteredTransformInitializer(
             ref_image, image, sitk.AffineTransform(3),
@@ -126,9 +143,10 @@ class DicomToXArray:
                                   sitk.sitkNearestNeighbor)
         new_mask = sitk.Resample(mask, ref_image, center,
                                  sitk.sitkNearestNeighbor)
-        return new_image, new_mask, metadata
+        return new_image, new_mask
 
-    def get_mask(self, SA, image):
+    def get_mask(self, SA):
+        image = self.raw_image_dict[SA]
         png_names = SA + '/*.png'
         tmp_mask_list = []
         for i, fn in enumerate(sorted(glob.glob(png_names))):
@@ -148,29 +166,12 @@ class DicomToXArray:
 
     def make_xarray(self):
         print(np.asarray(self.image_list).shape)
-        for a in self.metadata_list:
-            print(a)
 
-        zs = self.determine_best_zs()
+        zs = [self.loc_dict[SA][-1] for SA in self.good_SAs]
         self.xrds = xr.Dataset({'image': (['z', 't', 'y', 'x'], self.image_list),
                                 'mask': (['z', 't', 'y', 'x'], self.mask_list)},
-                               coords={'t': self.metadata_list[0][3],
-                                       'x': range(self.x_shape),
-                                       'y': range(self.y_shape),
-                                       # 'z': [a[2] for a in self.metadata_list]})
+                               coords={'t': self.timestamp_dict[list(self.good_SAs)[0]],
+                                       'x': range(self.ideal_x_shape),
+                                       'y': range(self.ideal_y_shape),
                                        'z': zs})
         self.xrds = self.xrds.sortby(['t', 'z'])
-
-    def determine_best_zs(self):
-        spacing_type1 = np.diff(np.sort(self.zs_type1))
-        spacing_type2 = np.diff(np.sort(self.zs_type2))
-        print(np.sort(self.zs_type1))
-        print(np.sort(self.zs_type2))
-
-        print(spacing_type1)
-        print(spacing_type2)
-
-        if np.std(spacing_type1) <= np.std(spacing_type2):
-            return self.zs_type1
-        else:
-            return self.zs_type2
